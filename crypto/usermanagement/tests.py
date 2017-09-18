9791fa554e433aaea5ba809367cb7f97ade50787
fkalebic <fkalebic@gmail.com>
2017-09-18 13:04:49 +0200

    Unit and integration tests for login

--- crypto/usermanagement/tests.py
+++
@@ -1,4 +0,0 @@
-""" Tests """
-from django.test import TestCase
-
-# Create your tests here.
---
+++ crypto/usermanagement/tests/tests.py
@@ -0,0 +1,37 @@
+from mixins import CreateUserMixin
+from django.test import TestCase
+
+
+class LoginAndLoginRedirectTest(CreateUserMixin, TestCase):
+
+    def setUp(self):
+        self.user = self.create_user('existing_user')
+
+    def test_redirection_to_login_on_anonymous_user_access_profile_page(self):
+        response = self.client.get('/usermanagement/')
+        self.assertRedirects(
+            response,
+            'login?next=/usermanagement/',
+             status_code=302,
+             target_status_code=301
+        )
+    
+    def test_login(self):
+        self.client.login(username = self.user.username, password = self.password)
+        response = self.client.get('/usermanagement/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'usermanagement/home.html')
+
+    def test_failed_login_redirect(self):
+        self.client.login(username = self.user.username, password = 'wrongpass')
+        response = self.client.get('/usermanagement/')
+        self.assertRedirects(
+            response,
+            'login?next=/usermanagement/',
+             status_code=302,
+             target_status_code=301
+        )
+
+    def test_failed_login(self):
+    	response = self.client.post('/login/', {'username': self.user.username, 'password': 'wrongpass'})
+    	self.assertTemplateUsed(response, 'registration/login.html')
